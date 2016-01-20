e9a7740e70efc46de5ef7fa8df8ec6183b89a65b
Xiaodong Wang <409681075@qq.com>
2016-01-20 21:44:51 +0800

    python file

---
+++ 1.py
@@ -0,0 +1,20 @@
+class Person:
+    def __init__(self, name, phone=None, email=None):
+        self.name = name
+        self.phone = phone
+        self.email = email
+
+
+class AddressBook:
+    ContactNum = 0
+    D = {}
+    
+    def __init__(self, name=None):
+        print('Create one Address_Book: \
+        the operation you can do is add, delete, find, export.')
+        self.name = name
+        print('This connect book is empty, please add.')
+
+    def add(self, Person p):
+        AddressBook.D[p.name] = [p.phone, p.email]
+        AddressBook.ContactNum += 1
